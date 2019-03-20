f2130d9db2225eda3882802a3abb01d7186138ee
Ancel Miller <ancelm@OMA-MAC-00132.local>
2019-03-20 14:19:28 -0700

    Adding build stuff

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import { graphql } from "gatsby";
+// import React from 'react';
+// import { graphql } from "gatsby";
 
-const Product = ({ data: { prismicProduct }}) => {
-  const { data } = prismicProduct;
-  return (
-    <>
-      <h1>Test</h1>
-    </>
-  )
-}
+// const Product = ({ data: { prismicProduct }}) => {
+//   const { data } = prismicProduct;
+//   return (
+//     <>
+//       <h1>Test</h1>
+//     </>
+//   )
+// }
 
-export default Product;
+// export default Product;
 
-export const query = graphql`
-  query ProductQuery {
-    prismicProduct {
-      uid
-      data {
-        title {
-          html
-          text
-        }
-        description {
-          html
-          text
-        }
-        images {
-          hero_image {
-            alt
-            copyright
-            url
-          }
-          tile_image {
-            alt
-            copyright
-            url
-          }
-        }
-      }
-    } 
-  }
-`;
+// export const query = graphql`
+//   query ProductQuery {
+//     prismicProduct {
+//       uid
+//       data {
+//         title {
+//           html
+//           text
+//         }
+//         description {
+//           html
+//           text
+//         }
+//         images {
+//           hero_image {
+//             alt
+//             copyright
+//             url
+//           }
+//           tile_image {
+//             alt
+//             copyright
+//             url
+//           }
+//         }
+//       }
+//     } 
+//   }
+// `;

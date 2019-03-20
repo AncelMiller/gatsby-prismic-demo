11e984d3fe71be52b3bb10cccd94215ba678fc84
Ancel Miller <ancelm@OMA-MAC-00132.local>
2019-03-20 15:49:11 -0700

    build issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-  require("dotenv").config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-})}
+})
 
 module.exports = {
   siteMetadata: {
